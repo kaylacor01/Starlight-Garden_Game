c50f455893a3334cd6349e16464ba4ddc699236a
Alondrag360 <alondrag4265@gmail.com>
2026-05-11 11:48:36 -0500

    Update ScoreManager.cs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,1 +1,28 @@
-h.
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour{
+  public static ScoreManager instance;
+  public TextMeshProUGUI scoreText;
+  private int score = 0;
+
+  void Awake(){
+    if(instance == null)
+      instance this;
+    else 
+      Destroy(gameObject);
+  }
+
+  void Start(){
+    UpdateScore();
+  }
+
+  public void AddPoint(int amount){
+    score += amount;
+    UpdateScore();
+  }
+
+  void UpdateScore(){
+    scoreText.text = "Score: " + score;
+  }
+}
