14c85945c48b889991d277f1a73e2a8c7a44d941
Alondrag360 <alondrag4265@gmail.com>
2026-05-11 11:28:05 -0500

    Update Collectible.cs

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,1 +1,12 @@
-h.
+using UnityEngine;
+
+public class Collectible : MonoBehaviour{
+  public int points = 1;
+
+  void OnTriggerEnter2D(Collider2D other){
+    if(other.CompareTag("player")){
+      ScoreManager.instance.AddPoint(points);
+      Destroy(gameObject);
+    }
+  }
+}
