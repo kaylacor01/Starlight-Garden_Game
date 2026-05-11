2d7685154daacd1ee01289bb2d7c557b1c2b6bb1
Alondrag360 <alondrag4265@gmail.com>
2026-05-11 11:41:29 -0500

    Update PauseMenu.cs

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,1 +1,32 @@
-h.
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour{
+  public GameObject PausePanel;
+
+  void Awake(){
+    if(PausePanel != null){
+      PausePanel.SetActive(false);
+    }
+  }
+
+  void OnEnable(){
+    GameManager.OnGameStateChanged += HandleGameStateChanged;
+  }
+
+  void OnDisable(){
+    GameManager.OnGameStateChanged -= HandleGameStateChanged;
+  }
+
+  void HandleGameStateChanged(GameManager.GameState state){
+    if (PausePanel != null)
+      PausePanel.SetActive(state == GameManager.GameState.Paused);
+  }
+
+  public void Resume(){
+    GameManager.Instance.ResumeGame();
+  }
+
+  public void GoToMainMenu(){
+    GameManager.Instance.GoToMainMenu();
+  }
+}
