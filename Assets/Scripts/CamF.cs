2c4f15bff1b17a9e569d6c4e60d3807f0ac4c6ff
Alondrag360 <alondrag4265@gmail.com>
2026-05-11 11:23:28 -0500

    Update CamF.cs

--- Assets/Scripts/CamF.cs
+++ Assets/Scripts/CamF.cs
@@ -1,1 +1,10 @@
-h.
+using UnityEngine;
+
+public class CamF : MonoBehaviour{
+  public Transform target;
+  public Vector3 offset;
+
+  void Update(){
+    transform.position = target.position + offset;
+  }
+}
